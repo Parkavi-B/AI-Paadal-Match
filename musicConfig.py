4dcceeacc7d5e24411e8331438aea67a3e4ca187
PARKAVI B <112197029+Parkavi-B@users.noreply.github.com>
2025-11-25 20:19:28 +0530

    Add files via upload

---
+++ musicConfig.py
@@ -0,0 +1,15 @@
+songs = {
+    "love": "Ennavale Adi Ennavale",
+    "sad": "Oora Therinjukitten",
+    "rain": "Mazhai Kuruvi",
+    "dance": "Monica",
+    "friendship": "Musthafa Musthafa",
+    "happy": "Anbe En Anbe",
+    "motivation": "Arjunar Villu",
+    "travel": "Kadhaippoma",
+    "calm": "Idhayathai Yedho Ondru",
+    "breakup": "Kaadhal En Kaviye"
+}
+
+# Default fallback song
+DEFAULT_SONG = "Thalapathy Kacheri"
